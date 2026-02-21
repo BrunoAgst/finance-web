93eab8ddb00fefa85a3d3feb4afc00fc14b12002
Bruno Augusto <bruno_agst@hotmail.com>
2026-02-21 14:01:32 -0300

    fix keycloak

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -41,8 +41,6 @@ export const AuthProvider = ({ children }) => {
       .init({
         onLoad: "check-sso",
         redirectUri: (import.meta.env.VITE_APP_URL || window.location.origin) + "/",
-        silentCheckSsoRedirectUri:
-          (import.meta.env.VITE_APP_URL || window.location.origin) + "/silent-check-sso.html",
         checkLoginIframe: false,
         pkceMethod: "S256",
         enableLogging: true,
